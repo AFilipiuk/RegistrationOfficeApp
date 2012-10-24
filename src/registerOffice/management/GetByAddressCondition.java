598e1e6bc1b20882a13032d40b08706e5e25e493
student <student@student-VirtualBox>
2012-10-24 17:45:33 +0200

    dodano pliki

---
+++ src/registerOffice/management/GetByAddressCondition.java
@@ -0,0 +1,19 @@
+package registerOffice.management;
+
+import registerOffice.businessObjects.persons.Person;
+
+public class GetByAddressCondition extends Condition<Person>{
+
+	private String address;
+	
+	public GetByAddressCondition(String address)
+	{
+		this.address=address;
+	}
+	@Override
+	protected boolean check(Person obj) {
+		// TODO Auto-generated method stub
+		return obj.getAddress().equalsIgnoreCase(address);
+	}
+
+}
---
+++ src/registerOffice/management/PersonManager.java
@@ -0,0 +1,46 @@
+package registerOffice.management;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import registerOffice.businessObjects.persons.Person;
+
+public class PersonManager implements ManagerInterface<Person>{
+
+	List<Person> persons= DatabaseContext.getPersons();
+	
+	@Override
+	public Person get(Condition<Person> condition) {
+		
+		for(Person p: persons)
+		{
+			if(condition.checkCondition(p))
+				return p;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public List<Person> getAll(Condition<Person> condition) {
+		
+		List<Person> results=new ArrayList<Person>();
+		for(Person p: persons)
+		{
+			if(condition.checkCondition(p))
+				results.add(p);
+		}
+		return results;
+	}
+
+	@Override
+	public boolean save(Person obj) {
+		return persons.add(obj);
+	}
+
+	@Override
+	public boolean delete(Person obj) {
+		return persons.remove(obj);
+	}
+
+}
